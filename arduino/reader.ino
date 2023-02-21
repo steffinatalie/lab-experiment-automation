d1bdf257338862bf9af84836e8e675b6a0dc03b0
steffinatalie <steffi.natalie7@gmail.com>
2023-02-21 15:18:37 +0700

    read mechanism testing

---
+++ arduino/reader.ino
@@ -0,0 +1,52 @@
+
+// Include the library:
+#include <SharpIR.h>
+
+// Define model and input pin:
+#define IRPin A1
+#define model 430
+
+// Create variable to store the distance:
+int distance_cm;
+
+/* Model :
+  GP2Y0A02YK0F --> 20150
+  GP2Y0A21YK0F --> 1080
+  GP2Y0A710K0F --> 100500
+  GP2YA41SK0F --> 430
+*/
+
+// Create a new instance of the SharpIR class:
+SharpIR mySensor = SharpIR(IRPin, model);
+
+void setup() {
+  // Begin serial communication at a baudrate of 9600:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // static state
+  // delay 30 menit
+
+  // maju
+
+  // baca
+  // delay 1 menit
+
+  // mundur
+
+
+
+  
+  // Get a distance measurement and store it as distance_cm:
+  distance_cm = mySensor.distance();
+
+  // Print the measured distance to the serial monitor:
+//  Serial.print("Mean distance: ");
+  Serial.print(distance_cm);
+  Serial.print('\n');
+//  Serial.println(" cm");
+
+  delay(1000);
+
+}
